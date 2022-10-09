5b343a7fbfc228fc403889890d8564773b667c0c
Bolakunmi <boluwayemi2000@gmail.com>
2022-10-09 23:45:47 +0100

    done

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,36 +1,75 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
-import { COUNTRY_LIST } from "../App";
+import { COUNTRY_LIST, LIGHT_DARK } from "../App";
 
 const COUNTRY = () => {
   const { numericCode } = useParams();
-  const {new_list} = useContext(COUNTRY_LIST);
-  const [selected, setSelected] = useState([]);
-
-
-  useEffect(() => {
-
-    const found = new_list.filter((country) => {
-      return country.numericCode.toString() === numericCode.toString();
-    });
-    setSelected(found);
-    console.log(selected);
+  const { list } = useContext(COUNTRY_LIST);
+  const { dark } = useContext(LIGHT_DARK);
+  const selected = list.find((country) => {
+    return country.numericCode.toString() === numericCode.toString();
   });
 
-  const CLICKED = () => { 
+
+  console.log(selected.borders);
+
+  return (
+    <div
+      className={dark ? " night_header selected" : " light_header selected"}
+      key={selected.numericCode}
+    >
+
+        <img alt="country flag" src={selected.flags.png} />
 
 
-    return (
-      <div className={dark ? " night_header country" : " light_header country"} key={selected.numericCode}>
+      <div className="details_container">
         <h4>{selected.name}</h4>
-        <p>population: {selected.population}</p>
-        <p>region: {selected.region}</p>
-        <p>capital: {selected.capital}</p>
-      </div>
-    );
-  };
+        <div className="details">
+          <div className="details1">
+            <p>
+              <span className="subtopics">Native Name: </span>
+              {selected.nativeName}
+            </p>
+            <p>
+              <span className="subtopics">population: </span>{" "}
+              {selected.population}
+            </p>
+            <p>
+              <span className="subtopics">Region: </span>
+              {selected.region}
+            </p>
+            <p>
+              <span className="subtopics">Sub Region: </span>
+              {selected.subregion}
+            </p>
+            <p>
+              <span className="subtopics">capital: </span>
+              {selected.capital}
+            </p>
+          </div>
 
-  return CLICKED;
+    
+          <div className="details2">
+            <p>
+              <span className="subtopics">Top Level Domain: </span>
+              {selected.topLevelDomain[0]}
+            </p>
+            <p>
+              <span className="subtopics">Currencies: </span>
+              {selected.currencies[0].name}
+            </p>
+            <p><span className="subtopics">Languages: </span><span className="languages">{selected.languages.map((lang)=><span> {lang.name}</span>)}</span></p>
+          </div>
+        </div>
+
+        <span className="border_countries"><h4>Border Countries:  </h4><span className="border_container">   
+           {selected.borders.map((bor)=> <span className="border">{bor}</span>)} 
+        </span></span>
+        
+
+      </div>
+    </div>
+  );
 };
 
 export default COUNTRY;
--- src/components/General.js
+++ src/components/General.js
@@ -1,37 +1,14 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext} from "react";
 import COUNTRIES from "./Countries.js";
-import { LIGHT_DARK } from "../App";
-import HEADER from "./Header.js";
-// import { SELECT_CONTINENT } from "../App";
+import { LIGHT_DARK, COUNTRY_LIST } from "../App";
+
 
 const GENERAL = () => {
   const { dark } = useContext(LIGHT_DARK);
-  const [my_search, setMy_search] = useState("");
-  const [search_empty, setSearch_empty] = useState(true);
-  // const {select_continent, setSelect_continent} = useContext(SELECT_CONTINENT);
+  const { search, setSearch, setSelectedContinent } =
+    useContext(COUNTRY_LIST)
 
-  const [select_continent, setSelect_continent] = useState("all");
 
-  useEffect(() => {
-    if (my_search.trim() === "") {
-      setSearch_empty(true);
-    } else {
-      setSearch_empty(false);
-    }
-
-    // console.log(search_empty)
-    // console.log(my_search.trim())
-  });
-
-  function orderDigits(e) {
-    if (e.target.value.match("a-zA-Z")) {
-      setMy_search(e.target.value);
-    } else {
-      console.log("wrong entry");
-      console.log(e.target.value);
-      // return setMy_search(my_search.substring(0, my_search.length-1))
-    }
-  }
 
   return (
     <div className="main-body">
@@ -54,24 +31,20 @@ const GENERAL = () => {
             placeholder="search for any country..."
             className={dark ? "night_header " : "light_header"}
             onChange={(e) => {
-              if (e.target.value.match("a-zA-Z+")) {
-                setMy_search(e.target.value);
-              } else {
-                setMy_search(e.target.value);
-              }
-              // setMy_search(e.target.value);
-              //   orderDigits(e)
+               setSearch(e.target.value);
             }}
             name="my_search"
+            value={search}
           />
         </div>
+
         <div>
           <select
             className={
               dark ? "night_header search_item" : "light_header search_item"
             }
             onChange={(e) => {
-              return setSelect_continent(e.target.value);
+              return setSelectedContinent(e.target.value);
             }}
           >
             <option value={"all"}>Filter by Region</option>
@@ -84,10 +57,8 @@ const GENERAL = () => {
         </div>
       </div>
 
-      {/* use an if condition for this */}
       <div className="country_display_style">
-        {/* {search_empty ? <COUNTRIES select_continent={select_continent} my_search={my_search}/> : <HEADER />} */}
-        <COUNTRIES select_continent={select_continent} my_search={my_search} />
+        <COUNTRIES />
       </div>
     </div>
   );
